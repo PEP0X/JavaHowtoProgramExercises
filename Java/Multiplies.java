388a44e19555b1deef9771aabc686dd844ade4d7
Saitama <onepercentpepo@gmail.com>
2023-05-04 23:23:12 +0300

    Second commit -> Put Sout message to enter nums

--- Java/Multiplies.java
+++ Java/Multiplies.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 class Multiplies{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First Number:");
         int n1 = scanner.nextInt();
+        System.out.println("Enter the Second Number:");
         int n2 = scanner.nextInt();
         if (n1%n2==0) {
             System.out.println(n1+"is a multiple of "+ n2 + " and the result is " + (n1 / n2));
